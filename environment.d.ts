e3e354a58696536e25c508f52708bd8c67232a3c
dhairya-bh <141953003+dhairya-bh@users.noreply.github.com>
2024-07-08 11:45:09 +0530

    change

--- environment.d.ts
+++ environment.d.ts
@@ -1,10 +1,10 @@
 declare namespace NodeJS {
   export interface ProcessEnv {
-    MYSQL_DB_HOST?: string;
-    MYSQL_DB_NAME?: string;
-    MYSQL_DB_PORT?: string;
-    MYSQL_DB_PASSWORD?: string;
-    MYSQL_DB_USERNAME?: string;
+    DATABASE_HOSTNAME?: string;
+    DATABASE_NAME?: string;
+    DATABASE_PORT?: string;
+    DATABASE_PASSWORD?: string;
+    DATABASE_USERNAME?: string;
     COOKIE_SECRET: string;
   }
 }
