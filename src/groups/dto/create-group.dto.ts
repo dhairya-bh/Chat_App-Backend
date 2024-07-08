f612ab9806ebe281496582483a35772f6a3d242e
dhairya-bh <141953003+dhairya-bh@users.noreply.github.com>
2024-07-08 11:25:05 +0530

    change

--- src/groups/dto/create-group.dto.ts
+++
@@ -1,9 +0,0 @@
-import { ArrayNotEmpty, IsString } from 'class-validator';
-
-export class CreateGroupDto {
-  @IsString({ each: true })
-  @ArrayNotEmpty()
-  users: string[];
-
-  title: string;
-}
